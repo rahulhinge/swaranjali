ef1d53f552adfaaf65e7c91f5862ae2d1eb89d2b
Rahul <rahulhinge2000@yahoo.com>
2021-10-30 22:20:12 +0100

    Kickoff - Empty Basic SpringBoot Project

---
+++ src/main/java/com/swar/swaranjali/SwaranjaliApplication.java
@@ -0,0 +1,13 @@
+package com.swar.swaranjali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaranjaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaranjaliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swar/swaranjali/SwaranjaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swar.swaranjali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaranjaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
